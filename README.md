6423279e07fdebfc9fb25867470d1fa639c4774f
kazetof <fukasawakaze@gmail.com>
2018-01-25 16:25:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# requirements
+- pydicom
+1.0 later
+You have to install from github to get 1.0.
---
+++ pathconfig.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+data_root = "/mnt/SSD1/share/CBIS_DDSM"
+data_root = Path(data_path)
